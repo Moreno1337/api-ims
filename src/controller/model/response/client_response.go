c86e74b0cc0ace80e392bbe701fc6fa42dd4f640
João Pedro Moreno <cjpm4000@hotmail.com>
2025-01-19 18:07:09 -0300

    File names updated to golang's convention. Added response/request models for clients

---
+++ src/controller/model/response/client_response.go
@@ -0,0 +1,28 @@
+package response
+
+import (
+	"time"
+)
+
+type ClientResponse struct {
+	Id                uint32
+	CompanyName       string
+	FantasyName       string
+	Cnpj              string
+	StateRegistration string
+	Cep               string
+	Sector            string
+	Number            string
+	Address           string
+	City              string
+	Neighborhood      string
+	Complement        string
+	State             string
+	PersonName        string
+	DateOfBirth       time.Time
+	Cpf               string
+	Gender            string
+	TelephoneNumber   string
+	CellPhoneNumber   string
+	Email             string
+}
