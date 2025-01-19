ae710e7127152772221344945178291209d9396a
João Pedro Moreno <cjpm4000@hotmail.com>
2025-01-19 17:17:29 -0300

    Added controllers for clients (business clients)

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Moreno1337/api-ims/src/controller/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("teste");
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	clientsGroup := router.Group("/clients")
+
+	routes.InitRoutes(clientsGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ src/controller/clients/getClient.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetClientsNaturalPerson(c *gin.Context) {}
+
+func GetClientsLegalEntity(c *gin.Context) {}
